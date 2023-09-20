5f648500f499bab6cb800aee54d8782f6ee7f4fe
md-mudassir <mdmudassirpro@gmail.com>
2023-09-20 23:06:34 +0530

    react scafolding

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
-  'h1',
+  "h1",
   {
-    id: 'title',
-    class: 'heading',
+    id: "title",
+    className: "heading",
   },
-  'Hello world from React!'
+  "Hello world from React!"
 );
 
 const parent = React.createElement(
-  'div',
-  { id: 'parent' },
+  "div",
+  { id: "parent" },
   [
     heading,
-    React.createElement('h1', {}, 'hello from h1'),
-    React.createElement('h2', {}, 'hello from h2'),
+    React.createElement("h1", {}, "hello from h1 🚀"),
+    React.createElement("h2", {}, "hello from h2"),
   ],
-  React.createElement('div', { id: 'parent2' }, [
+  React.createElement("div", { id: "parent2" }, [
     heading,
-    React.createElement('h1', {}, 'hello from h1'),
-    React.createElement('h2', {}, 'hello from h2'),
+    React.createElement("h1", {}, "hello from h1"),
+    React.createElement("h2", {}, "hello from h2"),
   ])
 );
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
