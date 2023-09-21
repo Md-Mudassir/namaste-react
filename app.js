71dab67d280d58f646e4c622b924330007369aa5
md-mudassir <mdmudassirpro@gmail.com>
2023-09-21 15:49:08 +0530

    refactor

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    className: "heading",
-  },
-  "Hello world from React!"
-);
+const parent = <h1>Hello React! 🚀</h1>;
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    heading,
-    React.createElement("h1", {}, "hello from h1 🚀"),
-    React.createElement("h2", {}, "hello from h2"),
-  ],
-  React.createElement("div", { id: "parent2" }, [
-    heading,
-    React.createElement("h1", {}, "hello from h1"),
-    React.createElement("h2", {}, "hello from h2"),
-  ])
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(parent);
